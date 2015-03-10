c20bc6cccbb011317445ecb39e95f39442ee2bcc
jlouros <jlouros@live.com>
2015-03-10 17:17:40 -0400

    adding DACPAC and Webdeploy scripts

---
+++ Visual Studio/SQL Server Projects/BatchDeploy-DACPACs.ps1
@@ -0,0 +1,92 @@
+﻿[CmdletBinding()]
+param(
+	[ValidateNotNullOrEmpty()]
+	[string]$serverInstance = "localhost"
+)
+
+Function Resolve-Invoke-Sqlcmd() {
+	if (-not (Get-Command "Invoke-SqlCmd" -ErrorAction SilentlyContinue))
+	{
+		Write-Verbose "Invoke-SqlCmd not found as a known command. Adding required PSSnapin"
+		Add-PSSnapin SqlServerCmdletSnapin100 
+		Add-PSSnapin SqlServerCmdletSnapin100 
+	}
+
+	if (-not (Get-Command "Invoke-SqlCmd" -ErrorAction SilentlyContinue)) {
+		Write-Error "Unable to resolve 'Invoke-SqlCmd' PowerShell command. This process can't continue without it!" -ErrorAction Stop
+	}
+}
+
+Function Get-SQLServerDataTools-Location {
+	<#
+	.SYNOPSIS
+		Gets path for SQL Server Data tools
+	
+	.DESCRIPTION
+		Gets path for SQL Server Data tools
+	#>
+
+
+	# Visual Studio 2012 SQL Package location
+	$SqlPackageLocation = "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe"
+
+	if((Test-Path $SqlPackageLocation) -eq $false) {
+		# Visual Studio 2013 SQL Package location
+		$SqlPackageLocation = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe"
+	}
+
+	if((Test-Path $SqlPackageLocation) -eq $false) {
+		# SQL Server 2014 SQL Package location
+		$SqlPackageLocation = "C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\sqlpackage.exe"
+	}
+
+	if((Test-Path $SqlPackageLocation) -eq $false) {
+		# SQL Server 2012 SQL Package location
+		$SqlPackageLocation = "C:\Program Files (x86)\Microsoft SQL Server\110\DAC\bin\sqlpackage.exe"
+	}
+
+	# Last verification point
+	if((Test-Path $SqlPackageLocation) -eq $false) {
+		Write-Host "'SqlPackage.exe' was not found on this machine."
+		Write-Error "Script execution terminated due missing required program 'SQL Server Data Tools'"  -ErrorAction Stop
+	}
+
+	return $SqlPackageLocation
+}
+
+$verbosityFlag = ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
+
+Resolve-Invoke-Sqlcmd
+
+$SqlPackageLocation = Get-SQLServerDataTools-Location
+
+$publishProfiles = Get-ChildItem . -Recurse -Include *.publish.xml
+
+
+
+Write-Verbose "Running 'DeleteMockedDatabases.sql' on '$serverInstance'..." 
+Invoke-Sqlcmd -InputFile $(Join-Path ($(Get-Item .).FullName) "DeleteMockedDatabases.sql") -ServerInstance $serverInstance -Verbose:$verbosityFlag
+
+
+
+Write-Verbose "Running 'CreateDatabases.sql' on '$serverInstance'..." 
+Invoke-Sqlcmd -InputFile $(Join-Path ($(Get-Item .).FullName) "CreateDatabases.sql") -ServerInstance $serverInstance -Verbose:$verbosityFlag
+
+Write-Verbose "Running 'CreateLinkedServers.sql' on '$serverInstance'..."
+Invoke-Sqlcmd -InputFile $(Join-Path ($(Get-Item .).FullName) "CreateLinkedServers.sql") -ServerInstance $serverInstance -Verbose:$verbosityFlag
+
+
+foreach($publishProf in $publishProfiles) 
+{
+	$dacpac = Get-ChildItem $publishProf.Directory -Recurse -Include "$($publishProf.Directory.Name).dacpac"
+
+	if($dacpac -eq $null) {
+		Write-Error "dacpac for '$($publishProf.Directory.Name)' was not found. Make sure the solution was built!" -ErrorAction Continue
+		continue
+	}
+
+	& $SqlPackageLocation /pr:"$($publishProf.FullName)" /sf:"$($dacpac.FullName)" /a:Publish 
+	Write-Output "-----------------------------------------------------------------------------"
+}
+
+cd $PSScriptRoot
---
+++ WebDeploy/Fetch-SetParameters.ps1
@@ -0,0 +1,77 @@
+﻿#Requires -Version 4.0
+
+Param(
+	[Parameter(Mandatory=$true)]
+	[string]$AppName,
+	[string]$TargetEnvironment = "localhost",
+	[string]$PackageLocation = $PSScriptRoot,
+	[string]$StashFilesRestUrl = "http://STASH_URL/rest/api/1.0/projects/POW/repos/REPOSITORY_NAME/files/ApplicationsEnvironmentSettings?limit=1000",
+	[string]$StashSettingsLocationUrl = "http://STASH_URL/projects/PROJECT_NAME/repos/REPOSITORY_NAME/browse/ApplicationsEnvironmentSettings/{0}?raw",
+	[switch]$AllowDefaultSettings
+)
+
+Function FindSettingsFileOnStash {
+	param(
+		[string]$TargetEnvironment,
+		[string]$AppName,
+		[string]$FilesStashRestUrl)
+
+	return (Invoke-RestMethod -Uri $FilesStashRestUrl -Method Get).values | where { $_ -match $AppName -and $_ -match $TargetEnvironment }
+}
+
+Function DownloadSettingsFileFromStash {
+	param(
+		[string]$settingsFile,
+		[string]$StashSettingsLocationUrl,
+		[string]$targetDeploymentFolder)
+
+	$targetFileLocation = Join-Path $targetDeploymentFolder $settingsFile
+
+	$webClient = New-Object System.Net.WebClient 
+	$fileDownloadUrl = [string]::Format($StashSettingsLocationUrl, $settingsFile)
+	$webClient.DownloadFile($fileDownloadUrl, $targetFileLocation)
+	$webClient.Dispose();
+}
+
+Write-Host "Initializing WebDeploy powershell script with the following input parameters:"
+Write-Host "   - Application name -> '$AppName'"
+Write-Host "   - Target environment -> '$TargetEnvironment'"
+Write-Host "   - Package location -> '$PackageLocation'"
+Write-Host "   - Stash files REST url -> '$StashFilesRestUrl'"
+Write-Host "   - Stash settings location url -> '$StashSettingsLocationUrl'"
+if($AllowDefaultSettings) {
+	Write-Host "   -> Allowing default settings. If no environment settings can be found, 'localhost' settings will be used instead!"
+}
+Write-Host "`r`n"
+
+if ((Test-Path $PackageLocation) -eq $false) { Write-Error "Unreachable 'package location'" -ErrorAction Stop }
+
+
+# Get Application deployment parameters location
+$settingsMatch = FindSettingsFileOnStash $TargetEnvironment $AppName $StashFilesRestUrl
+
+if($AllowDefaultSettings -and ($settingsMatch.Count -eq 0)) {
+	Write-Warning "Couldn't find '$TargetEnvironment'. Trying with 'localhost' settings instead!"
+
+	$settingsMatch = FindSettingsFileOnStash "localhost" $AppName $StashFilesRestUrl
+}
+
+if($settingsMatch.Count -eq 0) { Write-Error "Settings file not found. Verify your input parameters." -ErrorAction Stop }
+if($settingsMatch.Count -gt 1) { Write-Error "Too many file matches found. Please clean up our 'EnvironmentSettings' location." -ErrorAction Stop }
+
+
+# delete default SetParameters file
+$defaultSettingsFile = (Get-Item $PackageLocation).GetFiles() | where { $_.Name -match "SetParameters.xml" }
+Write-Host "Removing default '*SetParameters.xml' file which is '$defaultSettingsFile'`r`n"
+Remove-Item $defaultSettingsFile.FullName -ErrorAction Stop
+
+
+# copy the targetd SetParameters file
+Write-Host "Downloading settings file from Stash`r`n"
+DownloadSettingsFileFromStash $settingsMatch $StashSettingsLocationUrl $PackageLocation
+	
+
+# rename the targetd SetParameters file
+$newSettingsFile = (Get-Item $PackageLocation).GetFiles() | where { $settingsMatch.EndsWith($_.Name)  }
+Write-Host "Renaming environment specific '*SetParameters.xml' file with '$newSettingsFile' to '$defaultSettingsFile'"
+mv $newSettingsFile.FullName $defaultSettingsFile.FullName
